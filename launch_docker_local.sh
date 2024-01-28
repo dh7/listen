786190c3c0a05f54490f7a1fcfe7ee4605fc2d43
damien henry <dh7777777@gmail.com>
2024-01-28 18:40:46 +0100

    place dockerfile in the main folder to ease deployement

--- launch_docker_local.sh
+++ launch_docker_local.sh
@@ -1,4 +1,4 @@
-docker build -t listen_docker_image . -f docker/Dockerfile
+docker build -t listen_docker_image .
 docker stop listen_container
 docker rm listen_container
 docker run -it --name listen_container -p 8000:8000 listen_docker_image
